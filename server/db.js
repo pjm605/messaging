5773c1b1f6404739f7cc4b2ca5b8fe941a524f46
pjm605 <pjm605@gmail.com>
2017-01-25 07:51:47 -0500

    changed port

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@
 
 var Sequelize = require('sequelize');
 
-var databaseURI = 'postgres://mhpwwcctbmmrov:054953f5803466757f693995fd2f436e865e88fa6425287b9513dd6145805daf@ec2-54-235-90-107.compute-1.amazonaws.com:5432/dd3kqfc6s7d5eh';
-
+//var databaseURI = 'postgres://mhpwwcctbmmrov:054953f5803466757f693995fd2f436e865e88fa6425287b9513dd6145805daf@ec2-54-235-90-107.compute-1.amazonaws.com:5432/dd3kqfc6s7d5eh';
+var databaseURI = process.env.DATABASE_URL;
 var db = new Sequelize(databaseURI, {
   define: {
     timestamps: false,
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 var app = require('./app');
 var db = require('./db');
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function (err) {
   if (err) throw err;
