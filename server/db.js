1518c7f07f6d52290b6ebe94e7e473e6c25ea96c
pjm605 <pjm605@gmail.com>
2017-01-25 07:42:13 -0500

    update db url

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@
 
 var Sequelize = require('sequelize');
 
-var databaseURI = 'postgres://localhost:5432/hobby-dev';
+var databaseURI = 'postgres://mhpwwcctbmmrov:054953f5803466757f693995fd2f436e865e88fa6425287b9513dd6145805daf@ec2-54-235-90-107.compute-1.amazonaws.com:5432/dd3kqfc6s7d5eh';
 
 var db = new Sequelize(databaseURI, {
   define: {
