d29efe6513018cb3232428d9e7b0c47002c6343d
pjm605 <pjm605@gmail.com>
2017-01-25 10:04:17 -0500

    display style fixed

--- browser/app/display/display.ctrl.js
+++ browser/app/display/display.ctrl.js
@@ -2,13 +2,14 @@
 
 app.controller('displayCtrl', function ($scope, message) {
 	if(message.email && message.phone) {
-		$scope.to = message.email + " & " + message.phone
-	} 
+		$scope.email = message.email
+		$scope.phone = message.phone
+	}
 	else if (message.email) {
-		$scope.to = message.email
+		$scope.email = message.email
 	}
 	else if (message.phone) {
-		$scope.to = message.phone
+		$scope.phone = message.phone
 	}
 
 	$scope.message = message.message
--- server/api/api.router.js
+++ server/api/api.router.js
@@ -4,7 +4,6 @@
 var router = require('express').Router();
 
 router.use('/message', require('./message/message.router'));
-// router.use('/sender', require('./sender/sender.router'));
 router.use('/twilio', require('./sender/twilio.router'));
 router.use('/mailgun', require('./sender/mailgun.router'));
 
